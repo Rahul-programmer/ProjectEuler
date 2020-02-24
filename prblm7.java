60d5728fbf2ac01e012e151f44ee4f5bd78ee196
Rahul-programmer <56593123+Rahul-programmer@users.noreply.github.com>
2020-02-24 22:42:11 +0530

    Update prblm7.java

--- prblm7.java
+++ prblm7.java
@@ -13,25 +13,46 @@ public class prblm7 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int Switch = 1;
 		int count =1;
-		for(int i =3;i>0;i++)
+		int prime =0;
+		while(Switch==1)
 		{
-			for(int j=2;j<=i;j++)
+			if(isprime(count))
 			{
-				if(i%j!=0)
-				{
-					count ++;
-				}
+				count = count+2;
+				prime = prime+1;
 			}
-			if(count ==10001)
+			else
 			{
-				System.out.println(i);
-				break;
-				
+				count = count+2;
 			}
+			if(prime==10001)
+			{
+				Switch =0;
+				System.out.println(count-2);
+			}
+			
+			
 		}
+		
+		
 		// TODO Auto-generated method stub
 
+	}
+	public static boolean isprime(int n)
+	{
+		int k = (int)Math.sqrt(n);
+		
+		for(int i=2;i<=k;i++)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+		
 	}
 
 }
