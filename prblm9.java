b37bc2a5733236432277c7735570d5082cfddc92
Rahul-programmer <56593123+Rahul-programmer@users.noreply.github.com>
2020-02-25 10:28:43 +0530

    Update prblm9.java

--- prblm9.java
+++ prblm9.java
@@ -1,3 +1,11 @@
+
+/*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.*/
 /**
  * 
  */
@@ -13,17 +21,17 @@ public class prblm9 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int limit =100;
-		for(int a=0;a<limit;a++) {
-			for(int b=a+1;b<limit;b++)
+		int limit =500;
+		int m,n,a,b,c;
+		for( m=1;m<limit;m++) {
+			for(n=(m+1);n<limit;n++)
 			{
-				for(int c =b+1;c<limit;c++)
+				a=(n*n)-(m*m);
+				b=2*m*n;
+				c=(n*n)+(m*m);
+				if(a+b+c=1000)
 				{
-					if((a<b&&b<c&&a+b+c==limit)&&(c*c==a*a+b*b))
-					{
-						System.out.println(a*b*c);
-					}
-					
+					System.out.println(a*b*c);
 				}
 			}
 		}
@@ -32,3 +40,4 @@ public class prblm9 {
 	}
 
 }
+/*OUTPUT-31875000*/
