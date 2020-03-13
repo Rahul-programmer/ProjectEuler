f39bb4f70b61ad3cec3fe1fbc92482f7daf9afea
Rahul-programmer <56593123+Rahul-programmer@users.noreply.github.com>
2020-03-13 10:24:46 +0530

    Update prblm5.java

--- prblm5.java
+++ prblm5.java
@@ -6,6 +6,7 @@ What is the smallest positive number that is evenly divisible by all of the numb
  * 
  */
 package ProjectEluer;
+import java.math.BigInteger;
 
 /**
  * @author DELL
@@ -17,36 +18,31 @@ public class prblm5 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int smallest =0;
-		
-		
-		int flag;
-		for(int i =21;i>0;i++)
-		{ 
-			flag =1;
-			for(int j=2;j<=20;j++)
-			{
-				
-				if((i%j)!=0)
-				{
-					flag =0;
-		
-					
-				}
-				
-			}
-			if(flag ==1)
-			{
-				smallest = i;
-				break;
-			}
-		}
-		System.out.println(smallest);
+		prblm5 ob = new prblm5();
+	System.out.println(ob.multiple());
 
 		// TODO Auto-generated method stub
 
 	}
+	public static String multiple()
+	{
+		BigInteger allcm = BigInteger.ONE;
+		for(int i =1;i<=20;i++)
+		{
+			allcm = lcm(BigInteger.valueOf(i),allcm);
+		}
+		return allcm.toString();
+
+		
+	}
+	public static BigInteger lcm (BigInteger x,BigInteger y) {
+		return x.divide(x.gcd(y)).multiply(y);
+	
+	}
 
 }
+
+
+
 /*OUTPUT-232792560*/
 
