f80b4a9e9bc672102e35236ecc0ee5a8a3b19892
Rahul-programmer <56593123+Rahul-programmer@users.noreply.github.com>
2020-06-01 19:08:47 +0530

    Update prblm10.java

--- prblm10.java
+++ prblm10.java
@@ -5,7 +5,10 @@ Find the sum of all the primes below two million.*/
  * 
  */
 package ProjectEluer;
-import java.math.BigInteger;
+
+import java.util.ArrayList;
+
+
 
 /**
  * @author DELL
@@ -16,47 +19,45 @@ public class prblm10 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		BigInteger sum = new BigInteger("5");
-		
-		for(int i =5;i<2000000;i++)
-		{
-			if(isprime(i))
-			{
-				sum = sum.add(BigInteger.valueOf(i));
-			}
-		}
-		System.out.println(sum);
-		
-	}
-	public static boolean isprime(int no)
-	{
-		if(no<=1)
-		{
-			return false;
-		}
-		if(no<=3)
-		{
-			return false;
-		}
-		if(no%2==0||no%3==0)
-		{
-			return false;
-		}
-		for(int i =5;i*i<=no;i=i+6)
-		{
-			
-			if(no%i==0||no%(i+2)==0)
-			{
-				return false;
-			}
-		}
-		return true;
-		
- 
-}
-		
-		
+		private static ArrayList<Long> listOfPrimes = new ArrayList<Long>();
 
-}
+	    public static void main(String args[]) {
+
+	        long count = 0;
+	        for (long i = 2; i < 2000000; i++) {
+	            if (isPrime(i)) {
+	                count += i;
+	                System.out.print(i + " ");
+	            }
+	            if (i % 1000 == 0) {
+	                System.out.println();
+	            }
+	        }
+
+	        System.out.println("\nTotal " + count);
+
+	    }
+
+	    private static boolean isPrime(long n) {
+
+	        String strFromN = new Long(n).toString();
+	        if ((strFromN.length() != 1) && (strFromN.endsWith("2") || strFromN.endsWith("4") || strFromN.endsWith("5") || strFromN.endsWith("6") || strFromN.endsWith("8"))) {
+	            return false;
+	        }
+
+	        for (Long num : listOfPrimes) {
+	            if (num > Math.sqrt(n)) {
+	                break;
+	            }
+	            if (n % num.longValue() == 0) {
+	                return false;
+	            }
+	        }
+
+
+	        listOfPrimes.add(new Long(n));
+	        return true;
+	    }
+	}
+
 /*OUTPUT-142913828922*/
